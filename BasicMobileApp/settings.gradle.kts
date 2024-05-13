d488484d663e3adaaf3416f7f67f3935b7a5b1b4
AgnesCorpuz <38449514+AgnesCorpuz@users.noreply.github.com>
2024-05-13 13:28:18 +0800

    Add POC

---
+++ BasicMobileApp/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven ( url = "https://genesysdx.jfrog.io/artifactory/genesys-cloud-android.prod/")
+    }
+}
+
+rootProject.name = "BasicMobileApp"
+include(":app")
